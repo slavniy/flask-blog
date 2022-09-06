83d8588f683d77b789e9970cdb1007b3c7e33175
slavniy <larionov92spb@gmail.com>
2022-09-06 13:45:39 +0300

    form rendering by fields

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'larionov92spb@gmail.com'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-#53-258
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[InputRequired()])
+    submit = SubmitField('Submit')
+
+
+# 58-258
 @app.route('/')
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    return render_template('index.html',form=NameForm(), current_time=datetime.utcnow())
+
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
